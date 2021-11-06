8e2df90a1d8fa673aa536ae3e38b662f2aab6578
Vitor Adriano <vradriano@hotmail.com>
2021-11-06 17:18:12 -0300

    criando e adicionando middleware

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { request } = require('express');
 const express = require('express')
 const app = express();
 
@@ -6,6 +7,20 @@ const { v4: uuidv4 } = require('uuid')
 app.use(express.json())
 
 const customers = [];
+
+function verifyIfExistsAccountCPF(req, res, next){
+  const { cpf } = req.params
+
+  const customer = customers.find((customer) => customer.cpf === cpf)
+
+  if(!customer){
+    return res.status(400).json({ error: "Customer not found" })
+  }
+
+  request.customer = customer; 
+
+  return next();
+}
 
 
 app.post('/account', (req, res) => {
@@ -27,14 +42,8 @@ app.post('/account', (req, res) => {
   return res.status(201).send();
 });
 
-app.get('/statement/:cpf', (req, res) => {
-  const { cpf } = req.params
-
-  const customer = customers.find((customer) => customer.cpf === cpf)
-
-  if(!customer){
-    return res.status(400).json({ error: "Customer not found!" })
-  }
+app.get('/statement/:cpf', verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = request; 
 
   return res.json(customer.statement);
 });
